184c42831ff6b10b6e43381b57ef35fe438c5d42
chenhewen <532531210@qq.com>
2015-03-20 18:24:24 +0800

    broken pipe

---
+++ src/Server.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.net.*;
+
+/**
+ * 
+ * @author chenhewen
+ * 2015-3-20
+ */
+public class Server {
+	
+	public static void main(String[] args) {
+		
+		ServerSocket serverSocket = null;
+		try {
+			serverSocket = new ServerSocket(5432);
+		} catch (IOException e) {
+			System.out.println(e);
+			System.exit(1);
+		}
+		
+		int i = 1;
+		while (true) {
+			Socket socket = null;
+			try {
+				socket = serverSocket.accept();
+				System.out.println("accept a new socket");
+				
+				ServerThread serverThread = ServerThread.getInstance();
+//				ServerThread serverThread = new ServerThread();
+				serverThread.addSocket(socket);
+				System.out.println("add a new socket");
+				if (!serverThread.isAlive()) {
+					serverThread.start();
+					System.out.println("server thread start");
+				} 
+				
+				System.out.println("接收了 第" + i + "个请求");
+				i++;
+			} catch (IOException e) {
+				System.out.println(e);
+			} finally {
+
+			}
+		}
+	}
+}
--- src/ServerThread.java
+++ src/ServerThread.java
@@ -1,29 +1,103 @@
-import java.io.*;
-import java.net.*;
- 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.PriorityBlockingQueue;
+
 public class ServerThread extends Thread {
-    static String hello = "From Server: Hello world";
-    Socket sock;
-    public ServerThread(Socket s)
-    {
-        sock =s ;
-    }
-    public void run()
-    {
-        try{
-            InputStream in = sock.getInputStream();
-            DataInputStream din = new DataInputStream(in);
-            String name = din.readUTF();
-            OutputStream out = sock.getOutputStream();
-            DataOutputStream dos = new DataOutputStream(out);
-            dos.writeUTF(hello+"your name :"+name);
-            in.close();
-            out.close();
-            sock.close();
-        }
-        catch(IOException e)
-        {
-            System.out.println(e);
-        }
-    }
+	static String hello = "From Server: Hello world";
+	
+	BlockingQueue<Socket> mSocketQueue = new LinkedBlockingQueue<Socket>();
+	
+	Map<String, ProtocolModle> mPool = new HashMap<String, ProtocolModle>();
+	
+	Worker mWorker = new Worker();
+	
+	List<Socket> mSocketList = new ArrayList<Socket>();
+	
+	private static ServerThread sInstance;
+	
+	public static ServerThread getInstance() {
+		if (sInstance == null) {
+			sInstance = new ServerThread();
+		}
+		return sInstance;
+	}
+	
+	private ServerThread() {		
+		
+	}
+	
+	
+	public void addSocket(Socket socket) {
+		mSocketQueue.add(socket);
+	}
+	
+	public void run() {
+		
+		while (true) {
+			
+			InputStream in = null;
+			DataInputStream din = null;
+			OutputStream out = null;
+			DataOutputStream dos = null;
+			
+			Socket socket = null;
+			try {
+				socket = mSocketQueue.take();
+				System.out.println("take a socket  from block queue");
+			} catch (InterruptedException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			
+			if (socket == null) {
+				continue;
+			}
+			
+			mSocketList.add(socket);
+			System.out.println("add a socket mSocketList");
+			for (Socket s : mSocketList) {
+				System.out.println("遍历socket");
+				try {
+					in = s.getInputStream();
+					din = new DataInputStream(in);
+					String modleString = din.readUTF();
+					ProtocolModle protocolModle = new ProtocolModle(modleString);
+					
+//					mWorker.work(protocolModle);
+					
+					out = s.getOutputStream();
+					dos = new DataOutputStream(out);
+					dos.writeUTF(mWorker.work(protocolModle));
+					
+				} catch (IOException e) {
+					System.out.println(e);
+				} finally {
+					/*if (in != null) {
+						try {
+							in.close();
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					}
+					if (out != null) {
+						try {
+							out.close();
+						} catch (IOException e) {
+						}
+					}*/
+				}
+				
+			}
+		}
+	}
 }
